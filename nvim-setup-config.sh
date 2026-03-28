7c6f4a3f8f95efdc6e4626ce1d4c5cb69344006a
Peaso <psoderstrom.pro@outlook.com>
2026-03-28 23:35:32 +0100

    Addera val i script

--- nvim-setup-config.sh
+++ nvim-setup-config.sh
@@ -1,8 +1,29 @@
-!#/bin/bash
+#!/bin/bash
 
 echo 'Raderar ~/.config/nvim/*'
 rm -rf ~/.config/nvim/*
 
+if [ $1 = 1 ]
+then
 
-echo 'Kopierar över nvim-konfigurationen till ~/.config/nvim/'
+echo 'Kopierar över nvim till ~/.config/nvim/'
+cp -r ./nvim/. ~/.config/nvim/ 
+
+elif [ $1 = 2 ]
+then
+
+echo 'Kopierar över nvim-lazy till ~/.config/nvim/'
 cp -r ./nvim-lazy/. ~/.config/nvim/ 
+
+
+elif [ $1 = 3 ]
+then
+
+echo 'Kopierar över init.lua till ~/.config/nvim/'
+cp ./init.lua ~/.config/nvim/ 
+
+else
+echo 'Inget alternativ valt.'
+echo ' 1, nvim 2,nvim-lazy 3,init.lua '
+
+fi
