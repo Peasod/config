64c474b353550865e0ec22b9f8f9e90d8f90cf96
Peaso <psoderstrom.pro@outlook.com>
2026-03-29 23:22:55 +0200

    Uppdatering setup-nvim-config.sh, omstrukturering

--- nvim-config/setup-nvim-config.sh
+++ nvim-config/setup-nvim-config.sh
@@ -1,34 +1,7 @@
 #!/bin/bash
 
-radera() {
-	echo 'Raderar ~/.config/nvim/*'
-	rm -rf ~/.config/nvim/*
-}
+printf "Välj neovim config:\n"
 
-read -p 'Välj 1, nvim 2,nvim-lazy 3,init.lua 4,typecraft: ' choice
+select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
+echo 'Raderar ~/.config/nvim/*' && rm -rf ~/.config/nvim/* && cd "$d" && echo "Kopierar $d till ~/.config/nvim/" && cp -r . ~/.config/nvim/
 
-case $choice in 
-	1)
-	radera
-	echo 'Kopierar över nvim till ~/.config/nvim/'
-	cp -r ./nvim/. ~/.config/nvim/ 
-	;;
-	2)
-	radera
-	echo 'Kopierar över nvim-lazy till ~/.config/nvim/'
-	cp -r ./nvim-lazy/. ~/.config/nvim/ 
-	;;
-	3)
-	radera
-	echo 'Kopierar över init.lua till ~/.config/nvim/'
-	cp ./init.lua ~/.config/nvim/ 
-	;;
-	4)
-	radera
-	echo 'Kopierar över typecraft.init.lua till ~/.config/nvim/'
-	cp ./typecraftEP1-init.lua ~/.config/nvim/  
-	;;
-	*)
-	echo 'Ogiltigt val'
-	;;
-esac
