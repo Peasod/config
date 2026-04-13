f3a72c7fabf3b89c1f6000678b4743e8a8dc9aa5
Peaso <psoderstrom.pro@outlook.com>
2026-04-13 21:09:28 +0200

    Abbr work

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,9 +1,9 @@
 # git
 abbr -a -- gs 'git status'
-abbr -a -- gc 'git commit -m '
-abbr -a -- gca 'git commit -am '
-abbr -a -- gsw 'git switch'
-abbr -a -- gb 'git branch'
+abbr -a --set-cursor='%' gc 'git commit -m "%"'
+abbr -a --set-cursor='%' gca 'git commit -am "%"'
+abbr -a -- gsw 'git switch '
+abbr -a -- gb 'git branch '
 abbr -a -- gp 'git pull'
 abbr -a -- gpu 'git push'
 abbr -a -- gd 'git diff'
