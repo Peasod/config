be9c385286be32e403eecc91912b7b6d2c0cab26
Peaso <psoderstrom.pro@outlook.com>
2026-04-13 20:55:13 +0200

    Adderat git abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,5 +1,11 @@
+# git
 abbr -a -- gs 'git status'
-abbr -a -- gc 'git commit'
+abbr -a -- gc 'git commit -m '
+abbr -a -- gca 'git commit -am '
 abbr -a -- gsw 'git switch'
 abbr -a -- gb 'git branch'
 abbr -a -- gp 'git pull'
+abbr -a -- gpu 'git push'
+abbr -a -- gd 'git diff'
+
+
