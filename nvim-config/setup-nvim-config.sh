bef623831f82399bd10cc5f6683cb85d7d9623e6
Peaso <psoderstrom.pro@outlook.com>
2026-04-15 23:00:52 +0200

    Ansible playbook för installation av Neovim och fish

--- nvim-config/setup-nvim-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "Välj neovim config:\n"
-
-select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
-echo 'Raderar ~/.config/nvim/*' && rm -rf ~/.config/nvim/* && cd "$d" && echo "Kopierar $d till ~/.config/nvim/" && cp -r . ~/.config/nvim/
-
